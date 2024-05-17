59962c9bdbfa46952eaf5fba9d35f3a9f6803c56
David Song <david.jh.song@gmail.com>
2024-05-17 16:28:44 +0900

    validator edit

--- src/store/validator.ts
+++ src/store/validator.ts
@@ -1,28 +1,28 @@
 const dataChecker = (days: number, area: string, name: string, type: string, fullPrice: string, grant: string, actualPrice: string, weeks: number) => {
-    if (days === 0) {
-        alert('서비스 기간 오류. 관리자에게 문의해 주세요');
+    if (type === '') {
+        alert('바우처 유형을 선택해 주세요');
+        throw new Error('바우처 유형 오류');
+    } else if (days === 0) {
+        alert('서비스 기간을 선택해 주세요');
         throw new Error('Days 오류');
-    } else if (area === '') {
-        alert('지역 오류. 관리자에게 문의해 주세요');
-        throw new Error('서비스 기간 오류');
-    } else if (name === '') {
-        alert('산모 이름 오류. 관리자에게 문의해 주세요');
-        throw new Error('산모 이름 오류');
-    } else if (type === '') {
-        alert('바우처 유형 오류. 관리자에게 문의해 주세요');
-        throw new Error('서비스 기간 오류');
-    } else if (fullPrice === '') {
-        alert('기본 서비스 금액 오류. 관리자에게 문의해 주세요');
-        throw new Error('서비스 기간 오류');
-    } else if (grant === '') {
-        alert('정부지원금 오류. 관리자에게 문의해 주세요');
-        throw new Error('서비스 기간 오류');
-    } else if (actualPrice === '') {
-        alert('본인부담금 오류. 관리자에게 문의해 주세요');
-        throw new Error('서비스 기간 오류');
     } else if (weeks === 0) {
         alert('서비스 기간 오류. 관리자에게 문의해 주세요');
         throw new Error('Weeks 오류');
+    } else if (area === '') {
+        alert('지역을 선택해 주세요.');
+        throw new Error('지역 오류');
+    } else if (name === '') {
+        alert('산모 이름 오류. 관리자에게 문의해 주세요');
+        throw new Error('산모 이름 오류');
+    } else if (fullPrice === '') {
+        alert('기본 서비스 금액 오류. 관리자에게 문의해 주세요');
+        throw new Error('기본 서비스 금액 오류');
+    } else if (grant === '') {
+        alert('정부지원금 오류. 관리자에게 문의해 주세요');
+        throw new Error('정부지원금 오류');
+    } else if (actualPrice === '') {
+        alert('본인부담금 오류. 관리자에게 문의해 주세요');
+        throw new Error('본인부담금 오류');
     }
 };
 
